0fc3c49fca303a7612bff1f01d3ea225a4b0171b
Meyliisss <meylishudayberdiyew05@gmail.com>
2026-05-03 22:10:19 +0300

    Added database directory for clean organization

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'databases' / 'user_data.sqlite3',
     }
 }
 
